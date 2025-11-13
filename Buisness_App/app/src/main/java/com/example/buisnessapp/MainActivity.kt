bebc8f2ea339d0d4a169ceaa0c35db04ff505e43
AravindR-K <aravind05.rk@gmail.com>
2025-11-13 22:10:27 +0530

    deleted unwanted duplicate files

--- Buisness_App/app/src/main/java/com/example/buisnessapp/MainActivity.kt
+++ Buisness_App/app/src/main/java/com/example/buisnessapp/MainActivity.kt
@@ -49,7 +49,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
 import com.example.buisnessapp.ui.theme.BuisnessAppTheme
 
 class MainActivity : ComponentActivity() {
